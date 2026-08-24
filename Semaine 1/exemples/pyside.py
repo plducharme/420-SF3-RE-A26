687d3df1e32b17ca4f7b5feeffbbe9acc384387d
Pier Luc Ducharme <dutch80@gmail.com>
2026-08-24 13:08:48 -0400

    modifs exemples Semaine 1

--- Semaine 1/exemples/pyside.py
+++ Semaine 1/exemples/pyside.py
@@ -3,25 +3,26 @@ from PySide6.QtWidgets import QMainWindow, QApplication, QPushButton
 import sys
 
 
+# On hérite de la classe QMainWindow pour la personnaliser. Cette classe permet de créer une fenêtre avec des widgets
+# prédéfinis.
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        # Titre de la fenetre
+        # Titre de la fenêtre
         self.setWindowTitle("PySide")
 
         bouton_fermer = QPushButton(text="Fermer")
-        # connecter l'evenement du click a la fonction close de la fenetre
+        # connecter l'événement du click à la fonction "close()" de la fenetre
         bouton_fermer.pressed.connect(self.close)
 
         self.setCentralWidget(bouton_fermer)
-        # Affichage de la fenetre
-        self.show()
 
 
-# Creation de l'application
+# Création de l'application
 app = QApplication(sys.argv)
-# Creation de la fenetre (la classe ci-haut)
+# Creation de la fenêtre (la classe ci-haut)
 w = MainWindow()
-
+# Affichage de la fenêtre
+w.show()
 # execution
 app.exec()
